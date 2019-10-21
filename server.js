8fef9729062051e0c2d8d3d266042fbf05654539
ali1129js <ali1129js@gmail.com>
2019-10-21 16:31:36 +0200

    init express server and dependencies

---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: Ali
+ * @Date:   2019-10-21T16:25:24+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2019-10-21T16:29:10+02:00
+ */
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/',(req,res)=>res.send('Hello World'))
+
+app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
