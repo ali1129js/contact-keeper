bd12014be6eb6dde0f56c0854103e084dd98098b
ali1129js <ali1129js@gmail.com>
2019-10-23 08:40:44 +0200

    jsonwebtoken working

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,13 @@
  * @Author: Ali
  * @Date:   2019-10-21T16:58:03+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-10-23T08:25:05+02:00
+ * @Last modified time: 2019-10-23T08:39:03+02:00
  */
 const express = require("express");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const router = express.Router();
 
 const User = require("../models/User");
@@ -34,7 +36,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        return res.stats(400).json({ msg: "User already exists" });
+        return res.status(400).json({ msg: "User already exists" });
       }
       user = new User({ name, email, password });
 
@@ -42,7 +44,22 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send("User saved");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
