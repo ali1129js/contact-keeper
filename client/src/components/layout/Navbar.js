43b261e7f8f29f49666db56c0243f0eb98c47c8c
ali1129js <ali1129js@gmail.com>
2019-10-25 11:33:46 +0200

    link on navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,15 +2,22 @@
  * @Author: Ali
  * @Date:   2019-10-25T11:01:07+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-10-25T11:07:45+02:00
+ * @Last modified time: 2019-10-25T11:32:12+02:00
  */
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+
+
 const Navbar = ({title,icon}) => {
   return (
     <div className="navbar bg-primary">
       <h1>  <i className={icon} /> {title}
       </h1>
+      <ul>
+        <li> <Link to='/'> Home </Link> </li>
+        <li> <Link to='/about'> About </Link>  </li>
+      </ul>
     </div>
   )
 }
