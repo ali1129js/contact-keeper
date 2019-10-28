bb49524703667f4f43e1cac2e7a7882546ecfaaa
ali1129js <ali1129js@gmail.com>
2019-10-28 09:22:19 +0100

    contact components and contact item

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,15 +2,19 @@
  * @Author: Ali
  * @Date:   2019-10-27T10:47:27+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-10-27T11:17:55+01:00
+ * @Last modified time: 2019-10-28T09:10:36+01:00
  */
 import React,{useContext} from 'react'
+import ContactItem from './ContactItem'
 import ContactContext from '../../context/contact/contactContext'
 const Contacts = () => {
   const contactContext = useContext(ContactContext)
   const { contacts } = contactContext
   return(
-<> {contacts.map(contact => <h3>{contact.name}</h3>)} </>
+  <>{contacts.map(contact =>
+    <ContactItem contact={contact} key={contact.id}/>
+     )}
+  </>
   )
 }
 export default Contacts
