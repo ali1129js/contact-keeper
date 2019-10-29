d22661d063e38f2a2f15064d7d770d3644916240
ali1129js <ali1129js@gmail.com>
2019-10-29 11:11:24 +0100

    css transition and transition group

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,9 +2,10 @@
  * @Author: Ali
  * @Date:   2019-10-27T10:47:27+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-10-29T10:49:10+01:00
+ * @Last modified time: 2019-10-29T11:10:06+01:00
  */
 import React,{useContext} from 'react'
+import {CSSTransition,TransitionGroup} from 'react-transition-group'
 import ContactItem from './ContactItem'
 import ContactContext from '../../context/contact/contactContext'
 const Contacts = () => {
@@ -14,15 +15,20 @@ const Contacts = () => {
     return <h4>Please add a contact</h4>
   }
   return(
-  <>
-  {filtered !== null
-    ? filtered.map(contact => (<ContactItem contact={contact} key={contact.id}/>)
-      )
-    : contacts.map(contact =>
-    (<ContactItem contact={contact} key={contact.id}/>)
-     )}
+  <TransitionGroup>
 
-  </>
+    {filtered !== null
+      ? filtered.map(contact => (
+        <CSSTransition key={contact.id} timeout={500} classNames="item">
+          <ContactItem contact={contact} />
+        </CSSTransition>)
+      )
+      : contacts.map(contact =>
+        (<CSSTransition key={contact.id} timeout={500} classNames="item">
+          <ContactItem contact={contact} />
+        </CSSTransition>)
+      )}
+  </TransitionGroup>
   )
 }
 export default Contacts
